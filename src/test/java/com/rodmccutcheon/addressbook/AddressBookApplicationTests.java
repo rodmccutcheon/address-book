11acbc27873801a93eeb171377af773af9332b82
Rod McCutcheon <rod.mccutcheon@ortechnologies.com.au>
2021-05-01 15:02:59 +1000

    initial commit

---
+++ src/test/java/com/rodmccutcheon/addressbook/AddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodmccutcheon.addressbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
